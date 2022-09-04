6473bda57497d34e376c7aa85a4660899c732e99
Alex <a27072010n@gmail.com>
2022-09-04 13:35:48 +0300

    Объявил кнопки

--- app/src/main/java/com/example/mybuttonandview/MainActivity.java
+++ app/src/main/java/com/example/mybuttonandview/MainActivity.java
@@ -1,14 +1,51 @@
 package com.example.mybuttonandview;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView screenTv;
+
+    private Button resetButton;
+    private Button deleteButton;
+    private Button multiplyButton;
+    private Button divideButton;
+    private Button plusButton;
+    private Button minusButton;
+    private Button percentButton;
+    private Button pointButton;
+    private Button equallyButton;
+
+    private Button nineButton;
+    private Button eightButton;
+    private Button sevenButton;
+    private Button sixButton;
+    private Button fiveButton;
+    private Button fourButton;
+    private Button threeButton;
+    private Button twoButton;
+    private Button oneButton;
+    private Button zeroButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_all);
+    }
+
+    private void init() {
+        screenTv = findViewById(R.id.screen_text_view);
+
+        resetButton = findViewById(R.id.reset_button);
+        deleteButton = findViewById(R.id.del_character_button);
+    }
+
+    public void ClickButton(View view) {
+
     }
 }
