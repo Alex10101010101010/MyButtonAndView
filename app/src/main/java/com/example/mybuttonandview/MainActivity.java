e6bbd4ca50db531109d833fff9575a162b5953d7
Alex <a27072010n@gmail.com>
2022-09-04 13:36:26 +0300

    Объявил кнопки

--- app/src/main/java/com/example/mybuttonandview/MainActivity.java
+++ app/src/main/java/com/example/mybuttonandview/MainActivity.java
@@ -36,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_all);
+
+        init();
     }
 
     private void init() {
